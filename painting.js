526ccf080d431cc1a9d4028e97d01944f347ac82
Bhanjo <hanjo1515@naver.com>
2020-08-13 11:41:29 +0900

    0813

--- painting.js
+++ painting.js
@@ -1,11 +1,19 @@
+//페인팅 베이스 코드
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
-//켄버스를 css상으로만 구현했지 프로그래밍상으로 구현 안했으니 구현해줃다.
-canvas.width = 500;
-canvas.height = 500;
+//캔버스 기본 색상, 사이즈 등 설정
+const INITIAL_COLOR = "black";
+const CANVAS_SIZE = 500;
 
-ctx.strokesStyle = "black"; //색상
+//켄버스를 css상으로만 구현했지 프로그래밍상으로 구현 안했으니 구현해줃다.
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokesStyle = INITIAL_COLOR; //색상
+ctx.fillStyle = INITIAL_COLOR; //채우기
 ctx.lineWidth = 2.5; //굵기
 
 let painting = false;
@@ -34,8 +42,18 @@ function onMouseMove(event) {
 
 }
 
-function onMouseDown(event) {
-    painting = true;
+//채우기or그리기 변경
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    } else {
+        
+    }
+}
+
+//마우스 우클릭 방지
+function handleCM(evnet) {
+    event.preventDefault();
 }
 
 if(canvas) {
@@ -43,4 +61,6 @@ if(canvas) {
     canvas.addEventListener("mousedown", startPainting); //클릭시 이벤트 발생
     canvas.addEventListener("mouseup", stopPainting); //버튼 뗄시 발생
     canvas.addEventListener("mouseleave", stopPainting); //켄버스 범위 벗어날떄 이벤트 발생
+    canvas.addEventListener("click", handleCanvasClick); //모드 변경 이벤트
+    canvas.addEventListener("contextmenu", handleCM);
 }
